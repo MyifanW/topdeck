db57f87ce299a46b205c7b6c26abd3d8386efb3a
Michael Wang <myifanw@gmail.com>
2014-04-27 19:44:22 -0400

    Merge branch 'master' of https://github.com/MyifanW/topdeck
    Conflicts:
    	www/profile.php

--- www/templ.php
+++ www/templ.php
@@ -42,6 +42,11 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+	
+	<script type="text/javascript">
+	<!-- put yo scripts here-->
+	</script>
+
   </head>
 
   <body>
